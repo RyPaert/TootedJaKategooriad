860fbe08bcdbd5b9bceaff3bb1d100ed68a9928a
Ry <thomashenrypaert@gmail.com>
2025-03-28 07:29:06 +0200

    productscontext, 2 models

---
+++ TootedJaKategooriad/Category.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TootedJaKategooriad
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string? Name { get; set; }
+
+        public virtual ObservableCollectionListSource<Product> Products { get; } = new();
+    }
+}
---
+++ TootedJaKategooriad/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TootedJaKategooriad
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string? Name { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+    }
+}
